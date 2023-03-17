ae9e871f1092a56997675248871de019032b03ec
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-16 18:46:06 -0700

    printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main -printf function sizeof
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-		printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
 			printf("Size of an int: %zu byte(s)\n", sizeof(int));
 				printf("Size of a long int: %zu byte(s)\n", sizeof(long));
 					printf("Size of a long long int: %zu byte(s)\n", sizeof(double));
