59879cb98da85c26405cccfcc35fb7f452ae0d66
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-17 18:45:19 -0700

    UpperLower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+nclude <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:Always 0
+ */
+int main(void)
+{
+	char str;
+
+	for (str = 'a'; str <= 'z'; str++)
+	{
+		putchar(str);
+	}
+	for (str = 'A'; str <= 'Z'; str++)
+	{
+		putchar(str);
+	}
+	putchar('\n');
+	return (0);
+}
