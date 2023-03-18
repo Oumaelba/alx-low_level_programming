6fb970684733957c59429fb3882bb895aa1f9b76
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-17 19:55:07 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - entering quote
- * Return: 0
+ * Return: 1
  */
 
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-		return (0);
+		return (1);
 }
