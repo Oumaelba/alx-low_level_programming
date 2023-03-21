c0c69648c129096d1cad96bef80163be9d368904
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-21 15:34:55 +0000

    first

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#inclue < main.h >
+/**
+ * main - function for putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar ('\n');
+	return (0);
+}
