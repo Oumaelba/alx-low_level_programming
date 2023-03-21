e055f8a97244adfb5b6dcc094acfb455a1a19b13
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-21 23:44:29 +0000

    add

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - lowercase
+ *@c: parameter
+ *Return: 0
+ */
+
+int _islower(int c)
+{
+	char c;
+
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+	_putchar('\n);
+
+}
