628115b975a2584d26cbfac756c6bebde8206284
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-21 16:37:06 +0000

    first

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - printing alphabets
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alpha;
+	for(alpha='a'; alpha<='z'; alpha++)
+	{ 
+	  _putchar(alpha);
+	}
+	_putchar('\n');
+	return (0);
+}	
