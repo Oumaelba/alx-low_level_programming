4599b75fd9b0ffe84bff31011c7a6ced7b11701b
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-21 22:59:19 +0000

    add

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
- * main - print x10
+ * print_alphabet_x10 - printing
  *
- *Return: 0
  */
-int main(void)
+
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);
+	int i;
+	char alpha;
+
+	for (int i = 0; i < 10; i++)
+{
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+{	_putchar(alpha);
+}
+}
 }
