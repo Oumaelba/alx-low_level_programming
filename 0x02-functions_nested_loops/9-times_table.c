ada5f3a3b690d04f4713dbe399d028d84614d153
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-22 02:50:23 +0000

    add

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - description
+ * Return: 0
+ */
+void times_table(void)
+{
+	int a, b, n;
+
+	for (a = 0; a <= 9; b++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			n = a * b;
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+				_putchar(n % 10 + '0');
+			}
+			else
+			{
+				if (b != 0)
+				{
+				_putchar(' ');
+				}
+				_putchar(n + '0');
+			}
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
