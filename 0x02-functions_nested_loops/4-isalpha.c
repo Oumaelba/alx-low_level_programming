bbc2b6f60c8dd4a6255f06890aa243276ac060e0
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-22 00:08:42 +0000

    add

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - description
+ *@c: para
+ *Return: 0
+ */
+
+int _isalpha(int c)
+{
+	if (c == 'C' || c == 'c')
+		return (1);
+	else
+		return (0);
+	_putchar('\n');
+}
