93ad90a961e692d7eaef2a429113ffc688d3b069
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-23 19:53:15 +0000

    add

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - desciption
+ * Return: 0
+ * @c: para
+ */
+int _isdigit(int c)
+
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
