a790f11c9290adcb8f22d8df3d29a65a5c4b2cc1
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-23 21:57:55 +0000

    add

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,18 +9,17 @@ void more_numbers(void)
 {
 	int i;
 	int num;
-
+	
 	for (i = 0; i < 10; i++)
 {
 	for (num = 0; num < 10; num++)
 {
-    	_putchar(num + '0');
+	_putchar(num + '0');
 }
-    for (num = 10; num < 15; num++)
-    {
-        _putchar(num / 10);
-        _putchar(num % 10);
-    }
+	for (num = 10; num < 15; num++)
+{
+	_putchar(num / 10);
+	_putchar(num % 10);
 }
-_putchar('\n');
+}
 }
