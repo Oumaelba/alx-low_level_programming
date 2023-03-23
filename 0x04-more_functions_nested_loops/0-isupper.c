158d665e65afd903dc6dad93ca85667d793e0729
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-23 18:48:11 +0000

    add

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* isupper - description
+* Return: 0
+*/
+int _isupper(int c)
+{
+    int c;
+    
+    if (c ='A', c >= 'Z')
+    {
+        return (1)
+    }
+    else 
+    {
+        return (0)
+    }
+}
