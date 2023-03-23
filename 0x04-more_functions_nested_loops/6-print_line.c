3fab7ad601a9906cf4346404187871e287bea738
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-23 23:09:20 +0000

    add

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - printing
+ *
+ */
+ void print_line(int n)
+ {
+    if (n < 0)
+    {
+        _putchar('\n');
+        return;
+    }
+    while (n--)
+        _putchar('_');
+    _putchar('\n');
+
+ }
