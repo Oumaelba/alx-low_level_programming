9901733e5081d73de8abbe74aab63f6cfca119a5
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-23 01:33:00 +0000

    add

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - description
+ * @n: para
+ * Return: 0
+ */
+
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (int i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+	}
+		else
+		{
+			for (int i = n; i > 98; i--)
+			{
+				printf("%d, ", i);
+			}
+		}
+		printf("98\n");
+}
