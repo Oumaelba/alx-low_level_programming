b74266cb350b2583c4c73acecf3713caa9a9aa53
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-23 20:45:01 +0000

    add

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - printing numbers
+ *
+ */
+ void print_numbers(void)
+{
+ int num;
+
+ for (num = 0; num < 10; num++)
+ {
+ _putchar(num);
+ }
+ _putchar('\n');
+}
