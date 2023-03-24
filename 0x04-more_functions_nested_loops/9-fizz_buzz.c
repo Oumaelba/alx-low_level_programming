a4aed7c0c9ddb8eb290c7e72d0b6a69aff0cf12c
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-24 02:47:35 +0000

    add

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,7 +6,7 @@
 int main (void)
 {
 int n;
-for (n = 1; n <= 100; n++)
+for (n = 1; n <= 99; n++)
 {
 	if
 		(n % 5 == 0 && n % 3 == 0)
@@ -25,6 +25,6 @@ for (n = 1; n <= 100; n++)
 	else 
 		printf("%d ", n);
 }
-printf("\n");
+printf("Buzz\n");
 return (0);
 }
