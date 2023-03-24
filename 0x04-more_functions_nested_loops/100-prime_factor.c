f2aa74c985d4369400f8e6782a07fde470789d53
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-24 23:08:17 +0000

    add

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - description
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long factor = 1;
+	unsigned long our = 612852475143;
+	unsigned long b;
+
+	while (factor < 152488798)
+	{
+		if  (our % factor == 0)
+		{
+			if (factor % 2 != 0)
+			{
+				b = factor;
+			}
+		}
+		factor++;
+	}
+	printf("%ld \n", b);
+}
