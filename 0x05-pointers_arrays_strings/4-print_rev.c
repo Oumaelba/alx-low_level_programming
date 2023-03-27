6aee91f5a75a87675e4740ab96e05e8d4a23088f
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-27 21:28:01 +0000

    add

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - descrip
+ * @s: para
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i], i != '\0')
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
+		_putchar s[i];
+	}
+}
