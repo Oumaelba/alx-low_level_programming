80fa0d9085fa7ab74e37076140ecea41605add28
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-27 23:12:47 +0000

    add

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - descrip
+ * @s: para
+ */
+void rev_string(char *s)
+{
+	char *i = s;
+    char *n = s + _strlen(s) - 1;
+    char tmp;
+
+    while (i < n) {
+        tmp = *i;
+        *i = *n;
+        *n = tmp;
+        i++;
+        n--;
+    }
+}
