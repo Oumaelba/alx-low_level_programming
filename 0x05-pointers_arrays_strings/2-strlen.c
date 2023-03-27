f2a3356713f3ed3d605b34d8b6564e4fd044f2a6
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-27 20:25:25 +0000

    add

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,14 @@
 #include "main.h"
 #include <string.h>
+/**
+ * _strlen - description
+ * @s: para
+ * Return: length
+ */
 
 int _strlen(char *s)
 {
-    int length = strlen(s);
-    return length;
+	int length = strlen(s);
+
+	return (length);
 }
