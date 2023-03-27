aa75265f2e9c3be24622969b6739cd56be7723f3
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-27 18:52:50 +0000

    add

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_putchar - writes the character c to stdout
+ *@c: parametor
+ *Return: Always 0.
+ */
+int _putchar(char c)
+{
+	putchar(c);
+	return (0);
+}
