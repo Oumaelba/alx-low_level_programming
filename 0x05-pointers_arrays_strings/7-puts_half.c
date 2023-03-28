9f9a3931c816784bac5f3912af8dd4772321e36f
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-28 02:41:35 +0000

    add

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,27 +6,27 @@
 void puts_half(char *str)
 {
 	int i;
-
-    i = 0;
-    while (str[i])
-        i++;
-    if ((i % 2) == 0)
-    {
-        i = i / 2;
-        while (str[i])
-        {
-            _putchar(str[i]);
-            i++;
-        }
-    }
-    else
-    {
-        i = (i + 1) / 2;
-        while (str[i])
-        {
-            _putchar(str[i]);
-            i++;
-        }
-    }
-    putchar('\n');
+	
+	i = 0;
+	while (str[i])
+		i++;
+	if ((i % 2) == 0)
+	{
+		i = i / 2;
+		while (str[i])
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	else
+	{
+		i = (i + 1) / 2;
+		while (str[i])
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	_putchar('\n');
 }
