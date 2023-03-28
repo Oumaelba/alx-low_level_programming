901a54e35b6ef04170574a3b2553a39a33a89d00
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-28 00:35:34 +0000

    add

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,10 +5,10 @@
  *Return: i
  */
 
- int _strlen(char *s)
+int _strlen(char *s)
 {
 	int i = 0;
-	
+
 	while (s[i] != '\0')
 	{
 		i++;
@@ -20,7 +20,7 @@ void rev_string(char *s)
 	char *i = s;
 	char *n = s + _strlen(s) - 1;
 	char tmp;
-	
+
 	while (i < n)
 	{
 		tmp = *i;
