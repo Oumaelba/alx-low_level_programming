68889e38fa63d09a50a135ee4c6f03221ea21af2
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-28 02:24:26 +0000

    add

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,14 +13,14 @@ void puts_half(char *str)
 	{
 		len++;
 	}
-	
+
 	if (len % 2 == 0)
 	{
 		n = len / 2;
 	}
 	else
 	{
-		n = (len - 1) / 2;
+		n = (len + 1) / 2;
 	}
 	for (i = n; i < len; i++)
 	{
