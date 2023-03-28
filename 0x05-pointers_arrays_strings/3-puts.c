29d95a3358776f3ce17a5779ea059b605046492a
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-28 00:16:43 +0000

    add

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _puts - description
  *@str: para:
@@ -7,13 +6,13 @@
 
 void _puts(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
-    _putchar('\n');
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 
 }
