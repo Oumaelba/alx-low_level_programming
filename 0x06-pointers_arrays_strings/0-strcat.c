e31d4c285d327b40337aa5eaa0e7c9b713a8d2eb
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-29 22:40:29 +0000

    add

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - description
+ * @dest: para
+ * @src: para
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	return (dest);
+}
