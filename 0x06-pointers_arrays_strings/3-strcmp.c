42e81e5499345abb4861d5c8ae036e3c00c01c28
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-30 21:33:20 +0000

    add

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - descript
+ * @s1: para 1
+ * @s2: para 2
+ * Return: comparaision
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] == s2[i])
+	{
+        if (s1[i] == '\0')
+        return (0);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
