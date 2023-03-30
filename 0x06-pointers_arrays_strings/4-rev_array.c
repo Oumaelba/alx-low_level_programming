d4ae33191714497d0db3687625d3cbfa51bae3a9
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-30 22:42:13 +0000

    add

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - descr
+ * @a: para
+ * @n: para
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j = n - 1;
+	int tmp;
+
+	for (i < j; i++, j--)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+}
