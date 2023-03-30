79d2c0cfbd0eeeb82a9075e43afd876b418b6bb7
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-30 20:03:24 +0000

    add

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncpy - descr
+ * @dest: para 1
+ * @src: para 2
+ * @n: para 3
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
