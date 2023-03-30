b760ebe2497557a4dc37964468716eba31082238
Oumaelba <elbachirioumaima16@gmail.com>
2023-03-30 21:52:58 +0000

    add

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,8 +12,8 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] == s2[i])
 	{
-        if (s1[i] == '\0')
-        return (0);
+		if (s1[i] == '\0')
+			return (0);
 		i++;
 	}
 	return (s1[i] - s2[i]);
