cd60e708fa11aa83901a79ba81351363aa7223b8
Oumaelba <elbachirioumaima16@gmail.com>
2023-04-03 22:34:20 +0000

    add

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,29 +1,34 @@
 #include "main.h"
-
+/**
+ * _strspn - descrip
+ * @s: para 1
+ * @accept: para 2
+ * Return: store
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-    int i = 0;
-    int j = 0;
-    int store = 0;
+	int i = 0;
+	int j = 0;
+	int store = 0;
 
-    while (s[i] != '\0')
-    {
-        while (accept[j] != '\0')
-        {
-          if (s[i] == accept[j])
-          {
-            store++;
-            break;
-          }
-          else
-            j++;
-        }
-        if (accept[j] == '\0')
-        {
-            break;
-        }
-      j = 0;
-      i++;
-    }
-  return(store);
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				store++;
+				break;
+			}
+			else
+				j++;
+		}
+		if (accept[j] == '\0')
+		{
+			break;
+		}
+		j = 0;
+		i++;
+	}
+	return (store);
 }
