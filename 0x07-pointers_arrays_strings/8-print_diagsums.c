79072f1684125675e39e1c0e0dc79faeb5dff64f
Oumaelba <elbachirioumaima16@gmail.com>
2023-04-04 08:54:33 +0000

    add

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,12 +17,11 @@ void print_diagsums(int *a, int size)
 		i = (size + i + 1);
 	}
     res2 = 0;
-	i = 0;
+    i = 0;
 	while (i < j - 1)
 	{
 		res2 = res2 + a[i];
 		i = size + i - 1;
 	}
-    printf("%d, ", res1);
-	printf("%d\n", res2);
+    printf("%d, %d\n", res1, res2);
 }
