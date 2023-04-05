8ea2b3328d5358d2ce961c7f0d05f0a5795db9e4
Oumaelba <elbachirioumaima16@gmail.com>
2023-04-05 21:19:19 +0000

    add

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - descripti
+ * @x: para1
+ * @y: para2
+ * Return: 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (0);
+	if (y == 1)
+		return (x);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
