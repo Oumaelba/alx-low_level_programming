8f0c06959491139b152803cc12177c618da09a4a
Oumaelba <elbachirioumaima16@gmail.com>
2023-04-06 01:28:13 +0000

    add

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,31 @@
 #include "main.h"
 /**
+ * is_prime_number_helper - descr
+ * @n: para
+ * Return: 0
+ */
+int is_prime_number_helper(int n, int i)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (i == 1)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (is_prime_number_helper(n, i - 1));
+}
+/**
  * is_prime_number - descr
  * @n: para
  * Return: 0
  */
 int is_prime_number(int n)
 {
-    int i;
-    
-	if (n <= 1)
-	{
-		return (0);
-	}
-	for (i = 2; i <= n / 2; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (is_prime_number_helper n, n - 1);
 }
