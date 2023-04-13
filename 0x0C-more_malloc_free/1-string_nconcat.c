4023a16c3434bfc7cd92b32f9a9b97f1e7a2cacd
Oumaelba <elbachirioumaima16@gmail.com>
2023-04-13 11:32:39 +0000

    add

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,7 +19,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = ("");
 	for (i = 0; s[i]; i++)
 		len++;
-	concat = malloc(sizeof char * (len + 1));
+	concat = malloc(sizeof (char) * (len + 1));
 	if (concat == NULL)
 		return (NULL);
 	len = 0;
