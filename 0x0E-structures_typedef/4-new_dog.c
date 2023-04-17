dcc4d383d0830b1e52721f708c1073854ff80c07
Oumaelba <elbachirioumaima16@gmail.com>
2023-04-17 11:57:03 +0000

    add

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -63,7 +63,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 	{
-		free(name);
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
