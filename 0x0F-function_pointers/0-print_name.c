63d30b753a2023e2cd280732413feccefafec660
Oumaelba <elbachirioumaima16@gmail.com>
2023-04-19 11:38:35 +0000

    add

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,10 @@
 #include <stdlib.h>
 #include "function_pointers.h"
+/**
+ * print_name - description
+ * @name: para1
+ * @f: para2
+*/
 void print_name(char *name, void (*f)(char *))
 {
 	 (name && f) ? f(name) : (void)0;
