86f92e300bb29af747bd27fd7a1b4710c00f5325
Oumaelba <elbachirioumaima16@gmail.com>
2023-04-19 12:12:31 +0000

    add

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNCP_H
 #define FUNCP_H
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
