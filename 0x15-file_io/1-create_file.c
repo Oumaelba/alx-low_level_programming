4adc2090236cfaaeb5c8bbdc5e433fafb9c7d902
Oumaelba <elbachirioumaima16@gmail.com>
2023-05-08 15:19:03 +0100

    create files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
+		for (; text_content[len];)
 			len++;
 	}
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
