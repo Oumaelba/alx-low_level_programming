a74e1b16fd627c693588de3ae16602f871a4d0ca
Oumaelba <elbachirioumaima16@gmail.com>
2023-05-09 07:03:54 +0100

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,8 +16,8 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98)
-		;
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n"
+		, argv[1]), exit(98);
 	buffer = malloc(1024);
 	r = read(file_from, buffer, 1024);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
