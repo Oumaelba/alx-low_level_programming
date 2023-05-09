53de37ab72dd5a618ae12f5e0b7d3484287c7054
Oumaelba <elbachirioumaima16@gmail.com>
2023-05-09 06:55:59 +0100

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,16 +13,11 @@ int main(int argc, char *argv[])
 	char *buffer;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98)
+		;
 	buffer = malloc(1024);
 	r = read(file_from, buffer, 1024);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
@@ -32,6 +27,7 @@ int main(int argc, char *argv[])
 		if (w == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buffer);
 			exit(99);
 		}
 		r = read(file_from, buffer, 1024);
@@ -40,13 +36,16 @@ int main(int argc, char *argv[])
 	if (c == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		free(buffer);
 		exit(100);
 	}
 	c = close(file_to);
 	if (c == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		free(buffer);
 		exit(100);
 	}
+	free(buffer);
 	return (0);
 }
