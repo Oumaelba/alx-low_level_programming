6ef5e11dc90a81f08770a9d188d6f279d34b703f
Oumaelba <elbachirioumaima16@gmail.com>
2023-05-11 17:24:54 +0100

    print b

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_binary - Prints the binary representation of an unsigned long integer.
+ * @num: The number to print in binary.
+ */
+void print_binary(unsigned long int num)
+{
+	unsigned long int temp = num;
+	int bit_count = 0;
+
+	while ((temp >>= 1) > 0)
+	{
+		bit_count++;
+	}
+
+	for (int i = bit_count; i >= 0; i--)
+	{
+		if ((num >> i) & 1)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+	}
+
+	if (num == 0)
+	{
+		_putchar('0');
+	}
+}
