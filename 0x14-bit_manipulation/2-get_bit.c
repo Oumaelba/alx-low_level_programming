fe0999b7a23bc8dcb47b421bdd33165393477232
Oumaelba <elbachirioumaima16@gmail.com>
2023-05-23 20:21:54 +0100

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - gives value of bit at idx
+ * @i: index to give value
+ * @b: number
+ * Return: value at idx or -1
+ */
+
+int get_bit(unsigned long int b, unsigned int i)
+{
+	int bit_per_byto;
+
+	bit_per_byto = 8;
+	unsigned int bits_in_longo = sizeof(b) * bit_per_byto;
+
+	if (i >= bits_in_longo)
+		return (-1);
+
+	return ((b >> i) & 1);
+}
