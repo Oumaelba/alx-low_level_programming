80059cdbd6becb947e025f208d2828e70c0a18b7
Oumaelba <elbachirioumaima16@gmail.com>
2023-06-19 13:36:12 +0100

    update

---
+++ 0x18-dynamic_libraries/0-strln.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - description
+ * @s: para
+ * Return: i
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
